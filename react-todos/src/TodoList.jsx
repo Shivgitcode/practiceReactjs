7d6af3b300474ce862e0f066d75f4664654aa127
shivansh <shivneeraj2004@gmail.com>
2023-11-28 23:04:08 +0530

    making react-todos

---
+++ react-todos/src/TodoList.jsx
@@ -0,0 +1,8 @@
+const initialTodos = [
+  { id: 1, text: "walk the dog", completed: false },
+  { id: 3, text: "walk the cat", completed: false },
+  { id: 4, text: "walk the fish", completed: true },
+  { id: 5, text: "walk the chickens", completed: false },
+];
+
+export default function TodoList() {}
