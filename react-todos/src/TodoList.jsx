f8e6a0b87121d8c77fd6020b87b66fa4ff265775
shivansh <shivneeraj2004@gmail.com>
2023-11-30 11:46:54 +0530

    made todo list with react js

--- react-todos/src/TodoList.jsx
+++ react-todos/src/TodoList.jsx
@@ -1,8 +1,82 @@
-const initialTodos = [
-  { id: 1, text: "walk the dog", completed: false },
-  { id: 3, text: "walk the cat", completed: false },
-  { id: 4, text: "walk the fish", completed: true },
-  { id: 5, text: "walk the chickens", completed: false },
-];
+import { useState } from "react";
+import List from "@mui/material/List";
+import TodoItem from "./todoItem";
+import TodoForm from "./TodoForm";
+import { useEffect } from "react";
 
-export default function TodoList() {}
+export default function TodoList() {
+  const getInitialData = () => {
+    const data = JSON.parse(localStorage.getItem("todos"));
+    if (!data) {
+      return [];
+    }
+    return data;
+  };
+  const [todos, setTodos] = useState(getInitialData);
+  const removeTodo = (id) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((t) => t.id !== id);
+    });
+  };
+  const toggleTodo = (id) => {
+    setTodos((prevTodos) => {
+      return prevTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed: !todo.completed };
+        } else {
+          return todo;
+        }
+      });
+    });
+  };
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+  const addTodo = (text) => {
+    setTodos((prevTodos) => {
+      return [
+        ...prevTodos,
+        { id: crypto.randomUUID(), text: text, completed: false },
+      ];
+    });
+  };
+  return (
+    <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+      {todos.map((todo) => {
+        return (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            removeTodo={removeTodo}
+            toggleTodo={toggleTodo}
+          ></TodoItem>
+        );
+      })}
+      <TodoForm addTodo={addTodo}></TodoForm>
+    </List>
+  );
+}
+
+// export default function CheckboxList() {
+//   const [checked, setChecked] = React.useState([0]);
+
+//   const handleToggle = (value: number) => () => {
+//     const currentIndex = checked.indexOf(value);
+//     const newChecked = [...checked];
+
+//     if (currentIndex === -1) {
+//       newChecked.push(value);
+//     } else {
+//       newChecked.splice(currentIndex, 1);
+//     }
+
+//     setChecked(newChecked);
+//   };
+
+//   return (
+//     <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+//       {[0, 1, 2, 3].map((value) => {
+//         })}
+//     </List>
+//   );
+// }
---
+++ react-todos/src/todoItem.jsx
@@ -0,0 +1,41 @@
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Checkbox from "@mui/material/Checkbox";
+import IconButton from "@mui/material/IconButton";
+import CommentIcon from "@mui/icons-material/Comment";
+import Delete from "@mui/icons-material/Delete";
+
+export default function TodoItem({ todo, removeTodo, toggleTodo }) {
+  const labelId = `checkbox-list-label-${todo.id}`;
+
+  return (
+    <ListItem
+      secondaryAction={
+        <IconButton
+          edge="end"
+          aria-label="comments"
+          onClick={() => removeTodo(todo.id)}
+        >
+          <Delete />
+        </IconButton>
+      }
+      disablePadding
+    >
+      <ListItemButton role={undefined} dense>
+        <ListItemIcon>
+          <Checkbox
+            edge="start"
+            checked={todo.completed}
+            tabIndex={-1}
+            disableRipple
+            inputProps={{ "aria-labelledby": labelId }}
+            onChange={() => toggleTodo(todo.id)}
+          />
+        </ListItemIcon>
+        <ListItemText id={labelId} primary={todo.text} />
+      </ListItemButton>
+    </ListItem>
+  );
+}
