33973ebcdbe3b41e4ae2eac1084b806357b9590c
shivansh <shivneeraj2004@gmail.com>
2023-12-11 00:25:39 +0530

    checkbox handling

--- forms-in-react/src/App.jsx
+++ forms-in-react/src/App.jsx
@@ -2,7 +2,12 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [input, setInput] = useState({ firstname: "", lastname: "" });
+  const [input, setInput] = useState({
+    firstname: "",
+    lastname: "",
+    comment: "",
+    isVisible: true,
+  });
 
   function handleChange(event) {
     const fname = event.target.name;
@@ -20,6 +25,7 @@ function App() {
           type="text"
           placeholder="first name"
           name="firstname"
+          value={input.firstname}
           onChange={handleChange}
         />
 
@@ -27,7 +33,30 @@ function App() {
           type="text"
           placeholder="last name"
           name="lastname"
+          value={input.lastname}
           onChange={handleChange}
+        />
+
+        <textarea
+          name="comment"
+          id=""
+          cols="30"
+          rows="10"
+          onChange={handleChange}
+          value={input.comment}
+          placeholder="enter your comment"
+        ></textarea>
+        <br />
+        <br />
+        <br />
+
+        <label htmlFor="isvisible">Am I visible</label>
+        <input
+          type="checkbox"
+          onChange={handleChange}
+          name="isVisible"
+          id="isvisible"
+          checked={input.isVisible}
         />
       </form>
     </div>
