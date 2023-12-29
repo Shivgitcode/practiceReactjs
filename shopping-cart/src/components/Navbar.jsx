cf0a54a5e8663813d9dc4df21523b8d894ecbdb3
shivansh <shivneeraj2004@gmail.com>
2023-12-29 18:24:36 +0530

    shopping-cart-project done

--- shopping-cart/src/components/Navbar.jsx
+++ shopping-cart/src/components/Navbar.jsx
@@ -1,25 +1,34 @@
 import { FaShoppingCart } from "react-icons/fa";
 import logo from "../assets/react.svg";
 import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function () {
+  const { cart } = useSelector((state) => state);
   return (
-    <div className="flex flex-row justify-between">
-      <NavLink to="/">
-        <div>
-          <img src={logo} alt="" />
-        </div>
-      </NavLink>
-      <div>
+    <div>
+      <nav className="flex justify-between items-center h-20 max-w-6xl mx-auto">
         <NavLink to="/">
-          <p>Home</p>
+          <div className="ml-5">
+            <img src={logo} alt="" />
+          </div>
         </NavLink>
-        <NavLink to="/cart">
-          <p>
-            <FaShoppingCart></FaShoppingCart>
-          </p>
-        </NavLink>
-      </div>
+        <div className="flex items-center font-medium text-slate-100 mr-5 space-x-6">
+          <NavLink to="/">
+            <p>Home</p>
+          </NavLink>
+          <NavLink to="/cart">
+            <div className="relative">
+              <FaShoppingCart className="text-2xl"></FaShoppingCart>
+              {cart.length > 0 && (
+                <span className="absolute -top-1 -right-2 bg-green-600 text-xs w-5 h-5 flex justify-center items-center animate-bounce rounded-full text-white">
+                  {cart.length}
+                </span>
+              )}
+            </div>
+          </NavLink>
+        </div>
+      </nav>
     </div>
   );
 }
--- shopping-cart/src/components/Product.jsx
+++ shopping-cart/src/components/Product.jsx
@@ -18,24 +18,41 @@ export default function Product({ post }) {
   };
 
   return (
-    <div>
+    <div className="flex flex-col items-center justify-between hover:scale-110 transition duration-300 ease-in shadow-lg hover:shadow-2xl gap-3 p-4 mt-10 ml-5 rounded-xl outline">
       <div>
-        <p>{post.title}</p>
+        <p className="text-gray-700 font-semibold text-lg text-left truncate w-40 ml-1">
+          {post.title}
+        </p>
       </div>
       <div>
-        <p>{post.description}</p>
+        <p className="w-40 text-gray-400 font-normal text-[10px] text-left">
+          {post.description.split(" ").slice(0, 10).join(" ") + "..."}
+        </p>
       </div>
-      <div>
-        <img src={post.image} alt="" />
+      <div className="h-[180px]">
+        <img src={post.image} alt="" className="h-full w-full" />
       </div>
-      <div>
-        <p>{post.price}</p>
+
+      <div className="flex justify-between gap-12 items-center w-full mt-5 ">
+        <div>
+          <p className="text-green-600 font-semibold ">${post.price}</p>
+        </div>
+        {cart.some((p) => p.id == post.id) ? (
+          <button
+            className="text-gray-700 border-2 border-gray-700 rounded-full font-semibold text-[12px] p-1 px-3 uppercase hover:bg-gray-700 hover:text-white transition-all duration-300 ease-in"
+            onClick={removeFromCart}
+          >
+            Remove Item
+          </button>
+        ) : (
+          <button
+            className="text-gray-700 border-2 border-gray-700 rounded-full font-semibold text-[12px] p-1 px-3 uppercase hover:bg-gray-700 hover:text-white transition-all duration-300 ease-in"
+            onClick={addToCart}
+          >
+            Add to Cart
+          </button>
+        )}
       </div>
-      {cart.some((p) => p.id == post.id) ? (
-        <button onClick={removeFromCart}>Remove Item</button>
-      ) : (
-        <button onClick={addToCart}>Add to Cart</button>
-      )}
     </div>
   );
 }
