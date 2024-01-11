4461012d343c676b10178dbb081418b8bb1dd315
shivansh <shivneeraj2004@gmail.com>
2024-01-11 23:47:19 +0530

    project complete css left

--- redux-practice/password-generator/src/App.jsx
+++ redux-practice/password-generator/src/App.jsx
@@ -3,17 +3,17 @@ import "./App.css";
 import { useDispatch } from "react-redux";
 import { setPassword } from "./redux/slices/PasswordSlice";
 import generateRandomPassword from "./index";
+import shufflePassword from "./index";
 
 function App() {
-
- 
   const dispatch = useDispatch();
   const password = useSelector((state) => state.password.value);
 
   return (
     <div>
-      <span>{password}</span>
-      <button onClick={() => dispatch(setPassword(generateRandomPassword()))}>
+      <h1>Generate Password</h1>
+      <div>{password}</div>
+      <button onClick={() => dispatch(setPassword(shufflePassword()))}>
         Generate
       </button>
     </div>
--- redux-practice/password-generator/src/index.js
+++ redux-practice/password-generator/src/index.js
@@ -1,39 +1,57 @@
-  function randomNumber(start,end){
-    return Math.floor(Math.random()*(end-start))+start
+function randomNumber(start, end) {
+  return Math.floor(Math.random() * (end - start)) + start;
+}
+
+function randomLowerCaseLetter() {
+  const str = randomNumber(97, 122);
+  // const value=
+  return String.fromCharCode(str);
+}
+
+function randomUpperCaseLetter() {
+  const str = randomNumber(65, 90);
+  return String.fromCharCode(str);
+}
+
+function randomNums() {
+  const str = randomNumber(48, 50);
+  return String.fromCharCode(str);
+}
+
+console.log(randomLowerCaseLetter());
+console.log(randomUpperCaseLetter());
+console.log(randomNums());
+
+const functions = [randomNums, randomLowerCaseLetter, randomUpperCaseLetter];
+console.log(functions);
+// console.log(functions[0]())
+
+function generateRandomPassword() {
+  let passwd = "";
+  const f = 1;
+  // console.log(functions);
+  for (let q = 0; q < functions.length; q++) {
+    let randomNumber = functions[q]();
+    passwd += randomNumber;
   }
 
-  function randomLowerCaseLetter(){
-    const str=randomNumber(97,122);
-    // const value=
-    return String.fromCharCode(str)
+  for (let q = 0; q < 8 - functions.length; q++) {
+    var c = randomNumber(0, 2);
+    let random = functions[c]();
+    passwd += random;
   }
 
-  function randomUpperCaseLetter(){
-    const str=randomNumber(65,90)
-    return String.fromCharCode(str)
+  return passwd;
+}
+
+function shufflePassword() {
+  var oldPass = generateRandomPassword();
+  var newPass = "";
+  for (let i = 0; i < oldPass.length; i++) {
+    var rnd = randomNumber(0, oldPass.length - 1);
+    newPass += oldPass[rnd];
   }
+  return newPass;
+}
 
-  function randomNums(){
-    const str=randomNumber(48,50)
-    return String.fromCharCode(str)
-  } 
-
-
-  console.log(randomLowerCaseLetter())
-  console.log(randomUpperCaseLetter())
-  console.log(randomNums())
-
-  const functions=[randomNums,randomLowerCaseLetter,randomUpperCaseLetter]
-  console.log(functions)
-  // console.log(functions[0]())
-
-  function generateRandomPassword() {
-    let passwd = "";
-    for (let i = 0; i <= functions.length; i++) {
-      let randomNumber = functions[i]();
-      passwd += randomNumber;
-    }
-    return passwd;
-  }
-
-export default generateRandomPassword
+export default shufflePassword;
